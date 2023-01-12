f51daded539ea7a0e659a6feabf79a423ee23410
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-01-11 22:03:33 -0500

    V4

--- src/class5/ForLoops2.java
+++ src/class5/ForLoops2.java
@@ -2,7 +2,10 @@ package class5;
 
 public class ForLoops2 {
     public static void main(String[] args) {
-
-       
+        for (int i = 1; i <20; i++) {
+            if (i%3!=0){
+                System.out.println(i);
+            }
+        }
     }
 }
--- src/class5/ForLoops3.java
+++ src/class5/ForLoops3.java
@@ -2,6 +2,14 @@ package class5;
 
 public class ForLoops3 {
     public static void main(String[] args) {
+        /*for (int i = 0; i <=10 ; i++) {
+            System.out.println("5 * "+i+" = "+(5*1));
+            
+        }*/
+        for (int i = 1; i <+10 ; i++) {
+            System.out.println("3 * "+i+" = "+(3*i));
+
+        }
 
     }
 }
