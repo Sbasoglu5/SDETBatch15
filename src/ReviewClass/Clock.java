6a3d8d72ac28142a6963a32a4c2163fe2c8c67cf
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-01-22 01:12:01 -0500

    01/22

--- src/ReviewClass/Clock.java
+++ src/ReviewClass/Clock.java
@@ -1,26 +1,23 @@
 package ReviewClass;
 
+
+
 public class Clock {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+       for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 10; j++) {
                 for (int k = 0; k < 6; k++) {
                     for (int l = 0; l < 10; l++) {
                         if (i == 2 && j > 3) {
                             break;
-                        }
-                        System.out.println(i + "" + j + " : " + k + l);
-
-                    }
+                                }
+                                 System.out.println(i +""+ j + ":" + k + l);
+                   }
                 }
 
             }
-
         }
 
     }
 }
-
-
-
 
--- src/class7/D2Arrays.java
+++ src/class7/D2Arrays.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class D2Arrays {
     public static void main(String[] args) {
         // a 2D array which can hold 3 one D arrays
-        // each of of size 3
+        // each of  size 3
         String[][] thomsonTrain=new String [3][3];
         String[] cabin0={"Adam","Zafar","Sam"};
         String[] cabin1={"Nabi","Saud","Anees"};
@@ -15,8 +15,13 @@ public class D2Arrays {
         thomsonTrain[1]=cabin1;
         thomsonTrain[2]=cabin2;
 
+
+//  or instead of above this is the short way aka matrix;
+//  String [][] thomsonTRain={{"A","B","C"},{"D","E","F"},{"G","H","J"}};
+
+
         System.out.println(thomsonTrain[2][1]);
-        //System.out.println(thomsonTrain[0]);
-//        System.out.println(Arrays.toString(thomsonTrain[0]));
+        //System.out.println(thomsonTrain[0]); won't show anything instead write below
+        System.out.println(Arrays.toString(thomsonTrain[0]));
     }
 }
